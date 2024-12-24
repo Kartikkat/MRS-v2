da4241e0e52733800023ad3b004411e6bf19da73
Karikkat <rs.sgvn@gmail.com>
2024-12-24 18:13:28 +0530

    app.py

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    poster_path = data.get('poster_path')
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+        return full_path
+    else:
+        return None
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -18,7 +21,11 @@ def recommend(movie):
     for i in distances[1:6]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
+        poster = fetch_poster(movie_id)
+        if poster:
+            recommended_movie_posters.append(poster)
+        else:
+            recommended_movie_posters.append("https://via.placeholder.com/150")  # Placeholder image
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
     return recommended_movie_names, recommended_movie_posters
